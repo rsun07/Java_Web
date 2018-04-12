fa10918e4223540f963b299abe0bbd4613ec6091
Xiaoming Sun <rsun2014@gmail.com>
2018-04-12 13:41:49 -0700

    create new module for session

---
+++ cookie-and-session/session/src/main/java/pers/xiaoming/javaweb/CreateServer.java
@@ -0,0 +1,15 @@
+package pers.xiaoming.javaweb;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CreateServer extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    }
+}
