48ae40b6e3dbade77191bd1b4d07d885db2efea1
Xiaoming Sun <rsun2014@gmail.com>
2018-04-12 15:19:10 -0700

    Session behavior when cookie disabled

--- cookie-and-session/session/src/main/java/pers/xiaoming/javaweb/CreateServer.java
+++ cookie-and-session/session/src/main/java/pers/xiaoming/javaweb/CreateServer.java
@@ -8,17 +8,14 @@ public class CreateServer extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String caller = req.getHeader("caller");
         String username = req.getParameter("username");
 
-        req.setAttribute("caller", caller + "_attribute");
         req.setAttribute("username", username + "_attribute");
 
         HttpSession session = req.getSession();
 
-        session.setAttribute("caller", caller + "_session");
         session.setAttribute("username", username + "_session");
 
-        resp.getWriter().print(String.format("caller is : %s, username is : %s", caller, username));
+        resp.getWriter().print("username is : " + username);
     }
 }
--- cookie-and-session/session/src/main/java/pers/xiaoming/javaweb/SessionGetServer.java
+++ cookie-and-session/session/src/main/java/pers/xiaoming/javaweb/SessionGetServer.java
@@ -11,25 +11,27 @@ public class SessionGetServer extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String callerAttri = req.getParameter("caller");
         String usernameAttri = req.getParameter("username");
 
         // Don't create a new session if session not exist
         HttpSession session = req.getSession(false);
 
-        String callerSession = null;
         String usernameSession = null;
         if (session != null) {
-            callerSession = (String) session.getAttribute("caller");
             usernameSession = (String) session.getAttribute("username");
         }
 
+        Class sessionClass = null;
+
+        if (session != null) {
+            sessionClass = session.getClass();
+        }
+
         String response = String.format(
-                "attribute caller is : %s \n"
-                + "attribute username is : %s \n"
-                + "attribute caller is : %s \n"
-                + "attribute username is : %s \n",
-                callerAttri, usernameAttri, callerSession, usernameSession );
+                "attribute username is : %s \n"
+                + "session username is : %s \n"
+                + "Session implementation class is, %s " ,
+                usernameAttri, usernameSession, sessionClass);
 
         resp.getWriter().println(response);
     }
