9003ed80890880da6ef72b33ee0bc035fe1517b5
Xiaoming Sun <rsun2014@gmail.com>
2018-04-12 14:03:56 -0700

    Create server for session

--- cookie-and-session/session/src/main/java/pers/xiaoming/javaweb/CreateServer.java
+++ cookie-and-session/session/src/main/java/pers/xiaoming/javaweb/CreateServer.java
@@ -1,15 +1,24 @@
 package pers.xiaoming.javaweb;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
 import java.io.IOException;
 
 public class CreateServer extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String caller = req.getHeader("caller");
+        String username = req.getParameter("username");
+
+        req.setAttribute("caller", caller);
+        req.setAttribute("username", username);
+
+        HttpSession session = req.getSession();
+
+        session.setAttribute("caller", caller);
+        session.setAttribute("username", username);
+
+        resp.getWriter().print(String.format("caller is : %s, username is : %s", caller, username));
     }
 }
