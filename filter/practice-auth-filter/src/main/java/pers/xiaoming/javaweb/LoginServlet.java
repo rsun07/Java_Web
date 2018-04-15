ef8050a11466ef47ef55aa1db0d749fe77c05924
Xiaoming Sun <rsun2014@gmail.com>
2018-04-15 11:16:41 -0700

    inprove log in

--- filter/practice-auth-filter/src/main/java/pers/xiaoming/javaweb/LoginServlet.java
+++ filter/practice-auth-filter/src/main/java/pers/xiaoming/javaweb/LoginServlet.java
@@ -4,6 +4,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -24,10 +25,12 @@ public class LoginServlet extends HttpServlet {
         if (username != null && userDao.containsKey(username)) {
             String password = req.getParameter("password");
             if (userDao.get(username).equals(password)) {
-                resp.sendRedirect(req.getContextPath() +"/user/user_welcome.jsp");
+                HttpSession session = req.getSession();
+                session.setAttribute(username, password);
+                resp.sendRedirect("/user/welcome.jsp");
             }
         }
         resp.getWriter().write("Log in fail, please try again");
-        resp.sendRedirect(req.getContextPath() + "/login.jsp");
+        resp.sendRedirect( "/login.jsp");
     }
 }
