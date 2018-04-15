e17edc3c4f9c8856d63c11b29165f37610d910ca
Xiaoming Sun <rsun2014@gmail.com>
2018-04-15 16:00:44 -0700

    Upload servlet

---
+++ servlet3.0/file-upload/src/main/java/pers/xiaoming/javaweb/UploadServlet.java
@@ -0,0 +1,26 @@
+package pers.xiaoming.javaweb;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+import java.io.IOException;
+
+@WebServlet("/upload")
+@MultipartConfig
+
+public class UploadServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String path = this.getServletContext().getRealPath("/file-storage");
+
+        Part part = req.getPart("uploaded-file");
+
+        // set file dir and name
+        part.write(path + "file.txt");
+    }
+}
---
+++ servlet3.0/file-upload/src/main/webapp/index.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: xiaoming.sun
+  Date: 4/15/18
+  Time: 3:50 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>File Upload</title>
+</head>
+<body>
+    <form action="" method="post" enctype="multipart/form-data">
+        File <input type="file" name="uploaded-file"> <br>
+        <input type="submit" value="upload">
+    </form>
+</body>
+</html>
